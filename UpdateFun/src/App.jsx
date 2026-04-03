1564e444d1c8d289edc8faa236953ac31c5349d5
vishalthombare <vishalthombare862586@gmail.com>
2026-04-03 09:45:40 +0530

    Added new file

---
+++ UpdateFun/src/App.jsx
@@ -0,0 +1,37 @@
+import { useState } from 'react'
+
+function App() {
+  const [car, setCar] = useState({ year:2024,
+                                  make:"Toyota",
+                                  model:"Fortuner"
+  }
+  );
+
+  function handleYearChange(e){
+    setCar({...car, year:e.target.value})
+  }
+   function handleMakeChange(e){
+    setCar({...car, make:e.target.value})
+  }
+   function handleModelChange(e){
+    setCar({...car, model:e.target.value})
+  }
+
+
+  return (
+    <>
+      <div>
+        <p>Your Car Specification</p>
+        <p>Year : {car.year}</p>
+        <p>Made By : {car.make}</p>
+        <p>Model : {car.model}</p>
+
+        <input type="number" value={car.year} onChange={handleYearChange}></input>
+        <input type="text" value={car.make} onChange={handleMakeChange}></input>
+        <input type="text" value={car.model} onChange={handleModelChange}></input>
+      </div>
+    </> 
+  )
+}
+
+export default App
